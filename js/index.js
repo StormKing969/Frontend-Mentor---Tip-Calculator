654a5c1a0fde95f79300d50ce8e031a81d69038c
StormKing969 <wijesinghesaj@gmail.com>
2021-08-14 12:46:15 -0400

    JavaScript Final Version

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
-// ----- Getting the % tip -----
+// ----- Getting the custom % tip -----
+
+document.querySelector(".custom_tip_percentage").addEventListener("click", function() {
+    var customTip = document.getElementById("custom-tip-percentage");
+    var newTipPercentage = parseInt(customTip.elements[0].value);
+    // console.log(newTipPercentage);
+
+    changeButtonColor();
+
+    var bill = Number(totalBillAmount());
+    // console.log(bill);
+    
+    var people = totalNumberOfPeople();
+    // console.log(people);
+
+    var combinedTip = calculateTotalTip(newTipPercentage, bill)
+    // console.log(combinedTip);
+    
+    var eachTip = (calculateTip(combinedTip, people)).toFixed(2);
+    if (eachTip == "NaN" || eachTip == "Infinity") {
+        eachTip = "0";
+    }
+    console.log(eachTip);
+
+    var eachTotal = (totalAmountEach(combinedTip, bill, people)).toFixed(2);
+    if (eachTotal == "NaN" || eachTotal == "Infinity") {
+        eachTotal = "0"
+    }
+    console.log(eachTotal);
+
+    document.getElementById("tip-amount").innerHTML = eachTip;
+    document.getElementById("total").innerHTML = eachTotal;
+})
+
+// ----- Getting the fixed % tip -----
 
 var tipType = document.querySelectorAll(".tip_percentage").length;
 // console.log(tipType);
@@ -16,16 +50,20 @@ for (var i = 0; i < tipType; i++) {
         var people = totalNumberOfPeople();
         // console.log(people);
 
-        
-
         var combinedTip = calculateTotalTip(valueInnerHTML, bill)
         // console.log(combinedTip);
         
         var eachTip = (calculateTip(combinedTip, people)).toFixed(2);
-        console.log(eachTip);
+        if (eachTip == "NaN" || eachTip == "Infinity") {
+            eachTip = "0";
+        }
+        // console.log(eachTip);
 
         var eachTotal = (totalAmountEach(combinedTip, bill, people)).toFixed(2);
-        console.log(eachTotal);
+        if (eachTotal == "NaN" || eachTotal == "Infinity") {
+            eachTotal = "0"
+        }
+        // console.log(eachTotal);
 
         document.getElementById("tip-amount").innerHTML = eachTip;
         document.getElementById("total").innerHTML = eachTotal;
@@ -77,6 +115,9 @@ function totalAmountEach(totalTip, amount, people) {
 document.querySelector(".reset_button").addEventListener("click", function() {
     document.getElementById("bill-form").elements[0].value = "";
     document.getElementById("num-people-form").elements[0].value = "";
+    document.getElementById("custom-tip-percentage").elements[0].value = "";
+    document.getElementById("tip-amount").innerHTML = "";
+    document.getElementById("total").innerHTML = "";
     resetButtonColor();
 })
 
